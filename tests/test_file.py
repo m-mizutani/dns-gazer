b39de9b41a27b8d7ea67f455b8c7840311be3956
Masayoshi Mizutani <mizutani@cookpad.com>
2017-12-26 18:58:24 +0900

    Fix test

--- tests/test_file.py
+++ tests/test_file.py
@@ -11,9 +11,7 @@ def test_output_to_stdout():
 
     # Get first message from iterator.
     msg = next(msgpack.Unpacker(bs))
-    assert msg[0] == b'banshee.dns.tx'
+    print(msg[0])
+    assert msg[0] == b'dns-gazer.dns.tx'
     assert msg[1] == 1444531212
     assert msg[2][b'client_addr'] == b'10.139.96.169'
-
-
-    
