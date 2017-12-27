559fc24f893ab391f1e1515fe2f567abc04cad72
Masayoshi Mizutani <mizutani@cookpad.com>
2017-12-27 11:47:39 +0900

    Refactor Parameter key

--- src/dns.cc
+++ src/dns.cc
@@ -2,22 +2,23 @@
 
 namespace dns {
 
-void Receiver::build_message(fluent::Message *msg, const pm::Property &p,
-                             bool without_query) {
-  auto is_query = (p.value(this->is_query_).uint() == 1);
+void Receiver::build_message(fluent::Message* msg, const pm::Property& p,
+                             const ParamKeySet& pks, bool without_query) {
+                             
+  auto is_query = (p.value(pks.is_query_).uint() == 1);
 
   if (is_query) {
-    msg->set("tx_id",       p.value(this->tx_id_).uint());
-    msg->set("client_addr", p.value(this->ipv4_src_).ip4());
-    msg->set("server_addr", p.value(this->ipv4_dst_).ip4());
+    msg->set("tx_id",       p.value(pks.tx_id_).uint());
+    msg->set("client_addr", p.value(pks.ipv4_src_).ip4());
+    msg->set("server_addr", p.value(pks.ipv4_dst_).ip4());
     msg->set("client_port", p.src_port());
     msg->set("server_port", p.dst_port());
     msg->set("query_ts",    p.ts_d());
     msg->set("status",      "no_reply");
   } else if (without_query) {
-    msg->set("tx_id",       p.value(this->tx_id_).uint());
-    msg->set("client_addr", p.value(this->ipv4_dst_).ip4());
-    msg->set("server_addr", p.value(this->ipv4_src_).ip4());
+    msg->set("tx_id",       p.value(pks.tx_id_).uint());
+    msg->set("client_addr", p.value(pks.ipv4_dst_).ip4());
+    msg->set("server_addr", p.value(pks.ipv4_src_).ip4());
     msg->set("client_port", p.dst_port());
     msg->set("server_port", p.src_port());
     msg->set("reply_ts",    p.ts_d());
@@ -29,17 +30,15 @@ void Receiver::build_message(fluent::Message *msg, const pm::Property &p,
   }
 
   auto rec_name = (is_query) ? "query" : "reply";
-  auto rec_map = msg->retain_map(rec_name);
+  auto rec_arr = msg->retain_array(rec_name);
   for (auto& tgt : this->targets_) {
     if (p.has_value(tgt->pid_)) {
       auto& arr =
           dynamic_cast<const pm::value::Array&>(p.value(tgt->pid_));
+      
       for (size_t i = 0; i < arr.size(); i++) {
-        std::stringstream ss;
-        ss << i;
-        auto r = rec_map->retain_map(ss.str());
         auto& rec = arr.get(i);
-
+        auto r = rec_arr->retain_map();
         r->set("section", tgt->name_);
         r->set("name", rec.find("name").repr());
         r->set("type", rec.find("type").repr());
@@ -58,30 +57,27 @@ void Receiver::build_message(fluent::Message *msg, const pm::Property &p,
 
 
 Receiver::Receiver(const pm::Machine& machine, fluent::Logger *logger) :
-    logger_(logger), cache_(600), prev_ts_(0) {
-  this->tx_id_    = machine.lookup_param_key("DNS.tx_id");
-  this->ipv4_src_ = machine.lookup_param_key("IPv4.src");
-  this->ipv4_dst_ = machine.lookup_param_key("IPv4.dst");
-  this->is_query_ = machine.lookup_param_key("DNS.is_query");
-
-
+    key_set_(machine), logger_(logger), cache_(600), prev_ts_(0)
+{
   auto push_record_param_key = [&](const std::string& tgt) {
     auto pid = machine.lookup_param_key(tgt);
     auto tp = new Target(tgt.substr(4), pid);
     this->targets_.push_back(tp);
   };
+
   push_record_param_key("DNS.question");
   push_record_param_key("DNS.answer");
   push_record_param_key("DNS.authority");
   push_record_param_key("DNS.additional");
 }
 
+
+
 void Receiver::recv(const pm::Property& p) {
   static const int TIMEOUT = 60;
 
-  auto is_query = (p.value(this->is_query_).uint() == 1);
-  auto key = new Key(p, this->tx_id_, this->ipv4_src_,
-                     this->ipv4_dst_, this->is_query_);
+  auto is_query = (p.value(this->key_set_.is_query_).uint() == 1);
+  auto key = new Key(p, this->key_set_);
                          
   // search expired query
   if (this->prev_ts_ > 0 && p.ts() > this->prev_ts_) {
@@ -102,7 +98,7 @@ void Receiver::recv(const pm::Property& p) {
       msg = this->logger_->retain_message("dns-gazer.dns.tx");
       msg->set_ts(p.ts());
 
-      this->build_message(msg, p);
+      this->build_message(msg, p, this->key_set_);
       this->cache_.put(TIMEOUT, *key, msg);
       msg = nullptr;
     }
@@ -113,11 +109,12 @@ void Receiver::recv(const pm::Property& p) {
       msg = dn.data();
       msg->set_ts(p.ts());
       this->cache_.remove(*key);
-      this->build_message(msg, p);
+      this->build_message(msg, p, this->key_set_);
     } else {
       msg = this->logger_->retain_message("dns-gazer.dns.tx");
       msg->set_ts(p.ts());
-      this->build_message(msg, p, true);  // without_query = true
+      // without_query = true
+      this->build_message(msg, p, this->key_set_, true);
     }
   }
 
@@ -127,8 +124,5 @@ void Receiver::recv(const pm::Property& p) {
 
   delete key;  
 }
-
-
-
 
 }
--- src/dns.hpp
+++ src/dns.hpp
@@ -4,30 +4,6 @@
 
 namespace dns {
 
-class Key : public tb::HashKey {
- public:
-  Key(const pm::Property &p, pm::ParamKey& p_tx_id, pm::ParamKey& p_src,
-      pm::ParamKey& p_dst, pm::ParamKey& p_is_query) {
-    auto f = [&](pm::ParamKey& pid) mutable {
-      size_t len;
-      const void *ptr = p[pid].raw(&len);
-      this->append(ptr, len);
-    };
-
-    f(p_tx_id);
-    if (p[p_is_query].uint() == 1) {
-      f(p_src);
-      f(p_dst);
-    } else {
-      f(p_dst);
-      f(p_src);
-    }
-
-    this->finalize();
-  }
-};
-
-
 struct Target {
   const std::string name_;
   const pm::ParamKey pid_;
@@ -35,24 +11,55 @@ struct Target {
       name_(name), pid_(pid) {}
 };
 
+struct ParamKeySet {
+  pm::ParamKey tx_id_, ipv4_src_, ipv4_dst_, is_query_;
+  ParamKeySet(const pm::Machine& machine) :
+      tx_id_(machine.lookup_param_key("DNS.tx_id")),
+      ipv4_src_(machine.lookup_param_key("IPv4.src")),
+      ipv4_dst_(machine.lookup_param_key("IPv4.dst")),
+      is_query_(machine.lookup_param_key("DNS.is_query")) {
+  }
+};
+
+class Key : public tb::HashKey {
+ public:
+  Key(const pm::Property &p, const ParamKeySet& pks) {
+    
+    auto f = [&](const pm::ParamKey& pid) mutable {
+      size_t len;
+      const void *ptr = p[pid].raw(&len);
+      this->append(ptr, len);
+    };
+
+    f(pks.tx_id_);
+    if (p[pks.is_query_].uint() == 1) {
+      f(pks.ipv4_src_);
+      f(pks.ipv4_dst_);
+    } else {
+      f(pks.ipv4_dst_);
+      f(pks.ipv4_src_);
+    }
+
+    this->finalize();
+  }
+};
+  
+
 
 class Receiver {
  private:
-
+  const ParamKeySet key_set_;
   std::vector<Target*> targets_;
   fluent::Logger *logger_;
   tb::LruHash<fluent::Message*> cache_;
   time_t prev_ts_;
-  pm::ParamKey tx_id_, ipv4_src_, ipv4_dst_, is_query_;
 
-  void build_message(fluent::Message *msg, const pm::Property &p,
-                     bool without_query = false);
+  void build_message(fluent::Message* msg, const pm::Property& p,
+                     const ParamKeySet& pks, bool without_query = false);
   
  public:
   Receiver(const pm::Machine& machine, fluent::Logger* logger);
-  void recv(const pm::Property& p);
-
-  
+  void recv(const pm::Property& p);  
 };
 
 }
