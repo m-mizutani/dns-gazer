3eaa59110c03c0341b53a2e948dc3c6774c2dee7
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2017-12-31 12:02:14 +0900

    Add text mode test

--- tests/test_file.py
+++ tests/test_file.py
@@ -2,6 +2,7 @@ import msgpack
 import io
 import tempfile
 import os
+import json
 
 import helper.proc
 
@@ -29,3 +30,24 @@ def test_output_to_file():
     assert msg[0] == b'dns-gazer.dns.tx'
     assert msg[1] == 1444531212
     assert msg[2][b'client_addr'] == b'10.139.96.169'
+    os.unlink(tpath)
+
+
+def test_output_to_textfile():
+    tfd, tpath = tempfile.mkstemp()
+    os.close(tfd)
+
+    out, pid = helper.proc.run(['-t', tpath])
+    assert out == b''
+    
+    lines = open(tpath, 'rt').read().split('\n')
+    assert len(lines) > 100
+    r1 = lines[0].split('\t')
+
+    assert r1[0] == '2015-10-11T02:40:12+00:00'
+    assert r1[1] == 'dns-gazer.dns.tx'
+    jdata = json.loads(r1[2])
+    assert jdata['client_addr'] == '10.139.96.169'
+    assert isinstance(jdata['query'], list)
+    assert isinstance(jdata['reply'], list)
+    os.unlink(tpath)
